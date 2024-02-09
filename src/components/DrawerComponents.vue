4d66b842e81ac441dfad6e4938c96b1d4ae1dd6f
unknown <avilasonson@gmail.com>
2024-02-09 21:14:31 +0800

    feat: add social icons

--- src/components/DrawerComponents.vue
+++ src/components/DrawerComponents.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="q-gutter-y-md">
-    <q-avatar size="220px">
-      <img src="/profile.png" />
-    </q-avatar>
-    <q-item-label class="text-center"
+    <div class="text-center">
+      <q-avatar size="220px">
+        <img src="/profile.png" />
+      </q-avatar>
+    </div>
+    <q-item-label class="quote text-center"
       >Still learning, always learning.</q-item-label
     >
+    <div class="q-gutter-x-sm text-center">
+      <q-avatar size="40px" color="white">
+        <img src="/facebook.svg" />
+      </q-avatar>
+      <q-avatar size="40px" color="white">
+        <img src="/github.svg" />
+      </q-avatar>
+      <q-avatar size="40px" color="white">
+        <img src="/instagram.svg" />
+      </q-avatar>
+      <q-avatar size="40px" color="white">
+        <img src="/linkedin.svg" />
+      </q-avatar>
+    </div>
+    <q-separator color="black" />
   </div>
 </template>
 <script>
@@ -13,4 +30,8 @@ export default {
   name: "DrawerComponents",
 };
 </script>
-<style></style>
+<style>
+.quote {
+  font-size: 18px;
+}
+</style>
