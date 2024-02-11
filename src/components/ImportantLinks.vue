6fbbafb76014e157af7e5339f1a7b127abb1afea
unknown <avilasonson@gmail.com>
2024-02-11 22:24:47 +0800

    add new component

---
+++ src/components/ImportantLinks.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-item-label>{{ name }}</q-item-label>
+</template>
+<script>
+export default {
+  name: "ImportantLinks",
+  props: {
+    name: String,
+    // link: String,
+  },
+};
+</script>
+<style></style>
