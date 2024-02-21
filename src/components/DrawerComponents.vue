3328d8dc879b7a6ff643dc1e65fe0202cf9a4ef2
unknown <avilasonson@gmail.com>
2024-02-21 20:12:45 +0800

    feat: add drawer components

---
+++ src/components/DrawerComponents.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="q-gutter-y-md">
+    <q-avatar size="220px">
+      <img src="/profile.png" />
+    </q-avatar>
+    <q-item-label class="text-center"
+      >Still learning, always learning.</q-item-label
+    >
+  </div>
+</template>
+<script>
+export default {
+  name: "DrawerComponents",
+};
+</script>
+<style></style>
