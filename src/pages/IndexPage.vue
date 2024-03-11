cb56744eb459f8107bcb40efc846f294193bdaae
unknown <avilasonson@gmail.com>
2024-03-11 18:51:40 +0800

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex">
+    <div class="text-h4">Erickson A. Dela Cruz</div>
+    <div class="text-subtitle1">Backend Web Developer</div>
+    <div>
+      <p>
+        I am a Backend Web/Mobile Developer with more than 1 year of experience
+      </p>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
