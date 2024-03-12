cb288f89d6cb41fc9d5e8054f977ea62e150f57f
unknown <avilasonson@gmail.com>
2024-03-12 22:44:56 +0800

    feat: add profile component

---
+++ src/components/ProfileComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="flex gray-bg">
+    <div class="q-pa-xl q-ml-lg">
+      <q-item-label class="text-h2">Hi, I'm {{ name }}</q-item-label>
+      <div class="q-mb-sm"></div>
+      <q-item-label class="text-subtitle1">{{ profession }}</q-item-label>
+      <div class="q-mb-md"></div>
+      <div class="justified">
+        <p>
+          {{ description }}
+          <br />
+          {{ specialization }}
+          <br />Want to know me better? Check out my
+          <b>{{ resumeLink }}</b>
+        </p>
+      </div>
+      <div class="q-mb-sm"></div>
+      <q-btn elevated color="green" label="Resume" />
+    </div>
+    <div class="q-pt-lg q-mt-md q-ml-auto q-mr-xl">
+      <q-avatar square size="300px">
+        <img :src="avatarUrl" />
+      </q-avatar>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileComponent",
+  props: {
+    name: String,
+    profession: String,
+    description: String,
+    specialization: String,
+    resumeLink: String,
+    avatarUrl: String,
+  },
+};
+</script>
+
+<style scoped>
+.gray-bg {
+  background-color: #f8f9fa;
+  height: 380px;
+}
+
+.justified {
+  text-align: start;
+  font-size: 16px;
+}
+</style>
