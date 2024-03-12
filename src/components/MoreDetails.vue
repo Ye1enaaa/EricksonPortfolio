cf1d49a34fc179e89fbf5c348d38f6aaf412bd8a
unknown <avilasonson@gmail.com>
2024-03-13 00:18:59 +0800

    chore :see_no_evil: make a readable code

---
+++ src/components/MoreDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="q-pt-md q-pl-lg q-ml-xl">
+    <q-item class="flex">
+      <div class="mini-line"></div>
+      <div class="q-ml-md"></div>
+      <q-item-label class="what-i-do">Who Am I?</q-item-label>
+    </q-item>
+    <div class="q-pb-sm"></div>
+    <q-item
+      >I am Erickson Dela Cruz, currently a 4th year BSIT student at University
+      of Science and Technology of Southern Philippines.<br />
+      I have one year of experience in Web and IoT development.
+    </q-item>
+  </div>
+</template>
+<script>
+export default {
+  name: "MoreDetails",
+};
+</script>
+<style scoped>
+.mini-line {
+  height: 38px;
+  width: 10px;
+  background-color: #40916c;
+}
+.what-i-do {
+  font-size: 30px;
+}
+</style>
