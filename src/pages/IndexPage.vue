53d69c2e5995cfc68cd60c171dfd0fa7b6149e07
unknown <avilasonson@gmail.com>
2024-03-13 00:18:42 +0800

    chore :see_no_evil: make a readable code

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,19 +1,47 @@
 <template>
-  <q-page class="flex">
-    <div class="text-h4">Erickson A. Dela Cruz</div>
-    <div class="text-subtitle1">Backend Web Developer</div>
-    <div>
-      <p>
-        I am a Backend Web/Mobile Developer with more than 1 year of experience
-      </p>
+  <div class="">
+    <profile-component
+      name="Erickson"
+      profession="Fullstack Web Developer"
+      description="I'm an Aspiring Software Engineer specialized"
+      specialization="frontend, backend, and internet of things development."
+      resumeLink="online resume"
+      avatarUrl="https://scontent.fmnl4-1.fna.fbcdn.net/v/t39.30808-6/428638466_3292971684336985_950038273816050461_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHVTjetQMV5ml2-9GwBC2VdvOC7nXVYo8i84LuddVijyBk9ZyIeBVLNcS7ypVFY5k31-kuyTbZoQGskie4SfNCR&_nc_ohc=L312sKcVwh4AX846E7a&_nc_ht=scontent.fmnl4-1.fna&oh=00_AfC_UG5EILArrC9pe8c0bDZZnT3EfvxF8JuLjkdxpIWTxw&oe=65F4B924"
+    />
+    <div class="flex">
+      <more-details />
     </div>
-  </q-page>
+  </div>
 </template>
 
+<style scoped>
+/* .gray-bg {
+  background-color: #f8f9fa;
+  height: 380px;
+}
+
+.justified {
+  text-align: start;
+  font-size: 16px;
+} */
+
+/* .mini-line {
+  height: 40px;
+  width: 10px;
+  background-color: #40916c;
+} */
+</style>
 <script>
+import MoreDetails from "src/components/MoreDetails.vue";
+import ProfileComponent from "src/components/ProfileComponent.vue";
 import { defineComponent } from "vue";
 
+//import mE from '/me.jpg';
 export default defineComponent({
   name: "IndexPage",
+  components: {
+    ProfileComponent,
+    MoreDetails,
+  },
 });
 </script>
