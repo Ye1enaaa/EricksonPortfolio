0e07df048aaa3717b5a2dc7c158c1403428f857a
unknown <avilasonson@gmail.com>
2024-03-13 13:07:29 +0800

    feat: add card label

---
+++ src/components/TechStackCardLabel.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="q-pt-sm q-pl-lg q-ml-xl q-gutter-y-sm">
+    <q-item class="tech-stack-label">
+      {{ roles }}
+    </q-item>
+  </div>
+</template>
+<script>
+export default {
+  name: "TechStackCardLabel",
+  props: {
+    roles: String,
+  },
+};
+</script>
+<style>
+.tech-stack-label {
+  font-size: 17px;
+}
+</style>
