8e8539673b59ccdd5db953df7913b6058e1dd51e
unknown <avilasonson@gmail.com>
2024-03-13 13:07:46 +0800

    feat: add tech stack card

---
+++ src/components/TechStacksCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-card class="my-card">
+    <q-img :src="image">
+      <div class="text-subtitle2 absolute-top text-center">{{ tech }}</div>
+    </q-img>
+  </q-card>
+</template>
+<script>
+export default {
+  name: "TechStacksCard",
+  props: {
+    image: String,
+    tech: String,
+  },
+};
+</script>
+<style scoped>
+.my-card {
+  width: 100%;
+  max-width: 150px;
+}
+</style>
